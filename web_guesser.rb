8f857b649ab94e4c1966b68cc0c66bbdbf401e47
Sailor Winkelman <sailor.winkelman@nike.com>
2019-11-07 12:35:44 -0800

    finish number guessing game

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,9 +1,33 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+SECRET_NUMBER = rand(100)
+
 get '/' do
+  number = SECRET_NUMBER
+  guess = params["guess"]
+  message = guess_checker(guess, number)
 
-  number = rand(10)
-  erb :index, :locals => {:number => number}
+  def guess_checker(guess, number)
+    guess = guess.to_i
 
+    if guess > number
+      "#{guess} is Too High"
+    elsif guess < number
+      "#{guess} is Too Low"
+    elsif guess === number
+      "Correct.
+      The Secret Number is: #{guess}!"
+    else
+      "#{there was an issue}"
+    end
+
+  end
+
+
+  erb :index, :locals => {
+    :number => number,
+    :guess => guess,
+    :message => message
+  }
 end
